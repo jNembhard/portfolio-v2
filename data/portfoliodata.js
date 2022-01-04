e03e2b6c8a5c4cb1affc546b830a0ffc79d48784
jNembhard <nembhardjl@outlook.com>
2022-01-03 23:46:17 -0500

    Updated portfolio photos and pathways

--- data/portfoliodata.js
+++ data/portfoliodata.js
@@ -1,96 +1,105 @@
 export const portfoliodata = [
   {
     id: 1,
-    name: "Manage",
-    slug: "manage",
+    name: "Coffeeroasters",
+    slug: "coffeeroasters",
     description:
       "This project required me to build a fully responsive landing page to the designs provided. I used HTML5, along with CSS Grid and JavaScript for the areas that required interactivity, such as the testimonial slider.",
     images: {
       index: {
-        mobile: "/assets/portfolio/mobile/image-portfolio-manage@2x.jpg",
-        tablet: "/assets/portfolio/tablet/image-portfolio-manage@2x.jpg",
-        desktop: "/assets/portfolio/desktop/image-portfolio-manage@2x.jpg",
+        mobile:
+          "/assets/portfolio/desktop/image-portfolio-coffeeroasters@2x.jpg",
+        tablet:
+          "/assets/portfolio/desktop/image-portfolio-coffeeroasters@2x.jpg",
+        desktop:
+          "/assets/portfolio/desktop/image-portfolio-coffeeroasters@2x.jpg",
       },
       detailhero: {
-        mobile: "/assets/detail/mobile/image-manage-hero@2x.jpg",
-        tablet: "/assets/detail/tablet/image-manage-hero@2x.jpg",
-        desktop: "/assets/detail/desktop/image-manage-hero@2x.jpg",
+        mobile: "/assets/detail/desktop/image-coffeeroasters-hero@2x.jpg",
+        tablet: "/assets/detail/desktop/image-coffeeroasters-hero@2x.jpg",
+        desktop: "/assets/detail/desktop/image-coffeeroasters-hero@2x.jpg",
       },
 
       preview: {
-        mobileOne: "/assets/detail/mobile/image-manage-preview-1@2x.jpg",
-        mobileTwo: "/assets/detail/mobile/image-manage-preview-2@2x.jpg",
-        tabletOne: "/assets/detail/tablet/image-manage-preview-1@2x.jpg",
-        tabletTwo: "/assets/detail/tablet/image-manage-preview-2@2x.jpg",
-        desktopOne: "/assets/detail/desktop/image-manage-preview-1@2x.jpg",
-        desktopTwo: "/assets/detail/desktop/image-manage-preview-2@2x.jpg",
+        mobileOne:
+          "/assets/detail/desktop/image-coffeeroasters-preview-1@2x.jpg",
+        mobileTwo:
+          "/assets/detail/desktop/image-coffeeroasters-preview-2@2x.jpg",
+        tabletOne:
+          "/assets/detail/desktop/image-coffeeroasters-preview-1@2x.jpg",
+        tabletTwo:
+          "/assets/detail/desktop/image-coffeeroasters-preview-2@2x.jpg",
+        desktopOne:
+          "/assets/detail/desktop/image-coffeeroasters-preview-1@2x.jpg",
+        desktopTwo:
+          "/assets/detail/desktop/image-coffeeroasters-preview-2@2x.jpg",
       },
     },
-    previous: "Fylo",
-    next: "Bookmark",
+    previous: "Netflix",
+    next: "Galleria",
   },
   {
     id: 2,
-    name: "Bookmark",
-    slug: "bookmark",
+    name: "Galleria",
+    slug: "galleria",
     description:
       "This project required me to build a fully responsive landing page to the designs provided. I used HTML5, along with CSS Grid and JavaScript for the areas that required interactivity, such as the features section.",
     images: {
       index: {
-        mobile: "/assets/portfolio/mobile/image-portfolio-bookmark@2x.jpg",
-        tablet: "/assets/portfolio/tablet/image-portfolio-bookmark@2x.jpg",
-        desktop: "/assets/portfolio/desktop/image-portfolio-bookmark@2x.jpg",
+        mobile: "/assets/portfolio/desktop/image-portfolio-galleria@2x.jpg",
+        tablet: "/assets/portfolio/desktop/image-portfolio-galleria@2x.jpg",
+        desktop: "/assets/portfolio/desktop/image-portfolio-galleria@2x.jpg",
       },
       detailhero: {
-        mobile: "/assets/detail/mobile/image-bookmark-hero@2x.jpg",
-        tablet: "/assets/detail/tablet/image-bookmark-hero@2x.jpg",
-        desktop: "/assets/detail/desktop/image-bookmark-hero@2x.jpg",
+        mobile: "/assets/detail/desktop/image-galleria-hero@2x.jpg",
+        tablet: "/assets/detail/desktop/image-galleria-hero@2x.jpg",
+        desktop: "/assets/detail/desktop/image-galleria-hero@2x.jpg",
       },
       preview: {
-        mobileOne: "/assets/detail/mobile/image-bookmark-preview-1@2x.jpg",
-        mobileTwo: "/assets/detail/mobile/image-bookmark-preview-2@2x.jpg",
-        tabletOne: "/assets/detail/tablet/image-bookmark-preview-1@2x.jpg",
-        tabletTwo: "/assets/detail/tablet/image-bookmark-preview-2@2x.jpg",
-        desktopOne: "/assets/detail/desktop/image-bookmark-preview-1@2x.jpg",
-        desktopTwo: "/assets/detail/desktop/image-bookmark-preview-2@2x.jpg",
+        mobileOne: "/assets/detail/desktop/image-galleria-preview-1@2x.jpg",
+        mobileTwo: "/assets/detail/desktop/image-galleria-preview-2@2x.jpg",
+        tabletOne: "/assets/detail/desktop/image-galleria-preview-1@2x.jpg",
+        tabletTwo: "/assets/detail/desktop/image-galleria-preview-2@2x.jpg",
+        desktopOne: "/assets/detail/desktop/image-galleria-preview-1@2x.jpg",
+        desktopTwo: "/assets/detail/desktop/image-galleria-preview-2@2x.jpg",
       },
     },
-    previous: "Manage",
-    next: "Insure",
+    previous: "Coffeeroasters",
+    next: "Planetfacts",
   },
   {
     id: 3,
-    name: "Insure",
-    slug: "insure",
+    name: "Planet Facts",
+    slug: "planetfacts",
     description:
       "This was a small project which mostly consisted of HTML and CSS. I built a fully-responsive landing page. The only JavaScript this project required was to enable the toggling of the mobile navigation.",
     images: {
       index: {
-        mobile: "/assets/portfolio/mobile/image-portfolio-insure@2x.jpg",
-        tablet: "/assets/portfolio/tablet/image-portfolio-insure@2x.jpg",
-        desktop: "/assets/portfolio/desktop/image-portfolio-insure@2x.jpg",
+        mobile: "/assets/portfolio/desktop/image-portfolio-planetfacts@2x.jpg",
+        tablet: "/assets/portfolio/desktop/image-portfolio-planetfacts@2x.jpg",
+        desktop: "/assets/portfolio/desktop/image-portfolio-planetfacts@2x.jpg",
       },
       detailhero: {
-        mobile: "/assets/detail/mobile/image-insure-hero@2x.jpg",
-        tablet: "/assets/detail/tablet/image-insure-hero@2x.jpg",
-        desktop: "/assets/detail/desktop/image-insure-hero@2x.jpg",
+        mobile: "/assets/detail/desktop/image-planetfacts-hero@2x.jpg",
+        tablet: "/assets/detail/desktop/image-planetfacts-hero@2x.jpg",
+        desktop: "/assets/detail/desktop/image-planetfacts-hero@2x.jpg",
       },
       preview: {
-        mobileOne: "/assets/detail/mobile/image-insure-preview-1@2x.jpg",
-        mobileTwo: "/assets/detail/mobile/image-insure-preview-2@2x.jpg",
-        tabletOne: "/assets/detail/tablet/image-insure-preview-1@2x.jpg",
-        tabletTwo: "/assets/detail/tablet/image-insure-preview-2@2x.jpg",
-        desktopOne: "/assets/detail/desktop/image-insure-preview-1@2x.jpg",
-        desktopTwo: "/assets/detail/desktop/image-insure-preview-2@2x.jpg",
+        mobileOne: "/assets/detail/desktop/image-planetfacts-preview-1@2x.jpg",
+        mobileTwo: "/assets/detail/desktop/image-planetfacts-preview-2@2x.jpg",
+        tabletOne: "/assets/detail/desktop/image-planetfacts-preview-1@2x.jpg",
+        tabletTwo: "/assets/detail/desktop/image-planetfacts-preview-2@2x.jpg",
+        desktopOne: "/assets/detail/desktop/image-planetfacts-preview-1@2x.jpg",
+        desktopTwo: "/assets/detail/desktop/image-planetfacts-preview-2@2x.jpg",
       },
     },
-    previous: "Bookmark",
-    next: "Fylo",
+    previous: "Galleria",
+    next: "Netflix",
   },
   {
     id: 4,
-    name: "Fylo",
-    slug: "fylo",
+    name: "Netflix Clone",
+    slug: "netflix",
     description:
       "This project was built in pure HTML and CSS. I had mobile and desktop designs to work to and build it so that it was fully responsive. I took a mobile-first approach and used modern CSS like Flexbox and Grid for layout purposes.",
     images: {
@@ -100,20 +109,20 @@ export const portfoliodata = [
         desktop: "/assets/portfolio/desktop/image-portfolio-fylo@2x.jpg",
       },
       detailhero: {
-        mobile: "/assets/detail/mobile/image-fylo-hero@2x.jpg",
-        tablet: "/assets/detail/tablet/image-fylo-hero@2x.jpg",
-        desktop: "/assets/detail/desktop/image-fylo-hero@2x.jpg",
+        mobile: "/assets/detail/desktop/image-netflix-hero@2x.jpg",
+        tablet: "/assets/detail/desktop/image-netflix-hero@2x.jpg",
+        desktop: "/assets/detail/desktop/image-netflix-hero@2x.jpg",
       },
       preview: {
-        mobileOne: "/assets/detail/mobile/image-fylo-preview-1@2x.jpg",
-        mobileTwo: "/assets/detail/mobile/image-fylo-preview-2@2x.jpg",
-        tabletOne: "/assets/detail/tablet/image-fylo-preview-1@2x.jpg",
-        tabletTwo: "/assets/detail/tablet/image-fylo-preview-2@2x.jpg",
-        desktopOne: "/assets/detail/desktop/image-fylo-preview-1@2x.jpg",
-        desktopTwo: "/assets/detail/desktop/image-fylo-preview-2@2x.jpg",
+        mobileOne: "/assets/detail/desktop/image-netflix-preview-1@2x.jpg",
+        mobileTwo: "/assets/detail/desktop/image-netflix-preview-2@2x.jpg",
+        tabletOne: "/assets/detail/desktop/image-netflix-preview-1@2x.jpg",
+        tabletTwo: "/assets/detail/desktop/image-netflix-preview-2@2x.jpg",
+        desktopOne: "/assets/detail/desktop/image-netflix-preview-1@2x.jpg",
+        desktopTwo: "/assets/detail/desktop/image-netflix-preview-2@2x.jpg",
       },
     },
-    previous: "Insure",
-    next: "Manage",
+    previous: "Planetfacts",
+    next: "Coffeeroasters",
   },
 ];
