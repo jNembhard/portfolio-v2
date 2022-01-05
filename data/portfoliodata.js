b5b70948ef75d94bdb7ddd5187fb82dabd1278ae
jNembhard <nembhardjl@outlook.com>
2022-01-04 20:55:06 -0500

    converted images to webp

--- data/portfoliodata.js
+++ data/portfoliodata.js
@@ -8,31 +8,31 @@ export const portfoliodata = [
     images: {
       index: {
         mobile:
-          "/assets/portfolio/desktop/image-portfolio-coffeeroasters@2x.jpg",
+          "/assets/portfolio/desktop/image-portfolio-coffeeroasters@2x.webp",
         tablet:
-          "/assets/portfolio/desktop/image-portfolio-coffeeroasters@2x.jpg",
+          "/assets/portfolio/desktop/image-portfolio-coffeeroasters@2x.webp",
         desktop:
-          "/assets/portfolio/desktop/image-portfolio-coffeeroasters@2x.jpg",
+          "/assets/portfolio/desktop/image-portfolio-coffeeroasters@2x.webp",
       },
       detailhero: {
-        mobile: "/assets/detail/desktop/image-coffeeroasters-hero@2x.jpg",
-        tablet: "/assets/detail/desktop/image-coffeeroasters-hero@2x.jpg",
-        desktop: "/assets/detail/desktop/image-coffeeroasters-hero@2x.jpg",
+        mobile: "/assets/detail/desktop/image-coffeeroasters-hero@2x.webp",
+        tablet: "/assets/detail/desktop/image-coffeeroasters-hero@2x.webp",
+        desktop: "/assets/detail/desktop/image-coffeeroasters-hero@2x.webp",
       },
 
       preview: {
         mobileOne:
-          "/assets/detail/desktop/image-coffeeroasters-preview-1@2x.jpg",
+          "/assets/detail/desktop/image-coffeeroasters-preview-1@2x.webp",
         mobileTwo:
-          "/assets/detail/desktop/image-coffeeroasters-preview-2@2x.jpg",
+          "/assets/detail/desktop/image-coffeeroasters-preview-2@2x.webp",
         tabletOne:
-          "/assets/detail/desktop/image-coffeeroasters-preview-1@2x.jpg",
+          "/assets/detail/desktop/image-coffeeroasters-preview-1@2x.webp",
         tabletTwo:
-          "/assets/detail/desktop/image-coffeeroasters-preview-2@2x.jpg",
+          "/assets/detail/desktop/image-coffeeroasters-preview-2@2x.webp",
         desktopOne:
-          "/assets/detail/desktop/image-coffeeroasters-preview-1@2x.jpg",
+          "/assets/detail/desktop/image-coffeeroasters-preview-1@2x.webp",
         desktopTwo:
-          "/assets/detail/desktop/image-coffeeroasters-preview-2@2x.jpg",
+          "/assets/detail/desktop/image-coffeeroasters-preview-2@2x.webp",
       },
     },
     previous: "Netflix",
@@ -46,22 +46,22 @@ export const portfoliodata = [
       "This project required me to build a fully responsive landing page to the designs provided. I used HTML5, along with CSS Grid and JavaScript for the areas that required interactivity, such as the features section.",
     images: {
       index: {
-        mobile: "/assets/portfolio/desktop/image-portfolio-galleria@2x.jpg",
-        tablet: "/assets/portfolio/desktop/image-portfolio-galleria@2x.jpg",
-        desktop: "/assets/portfolio/desktop/image-portfolio-galleria@2x.jpg",
+        mobile: "/assets/portfolio/desktop/image-portfolio-galleria@2x.webp",
+        tablet: "/assets/portfolio/desktop/image-portfolio-galleria@2x.webp",
+        desktop: "/assets/portfolio/desktop/image-portfolio-galleria@2x.webp",
       },
       detailhero: {
-        mobile: "/assets/detail/desktop/image-galleria-hero@2x.jpg",
-        tablet: "/assets/detail/desktop/image-galleria-hero@2x.jpg",
-        desktop: "/assets/detail/desktop/image-galleria-hero@2x.jpg",
+        mobile: "/assets/detail/desktop/image-galleria-hero@2x.webp",
+        tablet: "/assets/detail/desktop/image-galleria-hero@2x.webp",
+        desktop: "/assets/detail/desktop/image-galleria-hero@2x.webp",
       },
       preview: {
-        mobileOne: "/assets/detail/desktop/image-galleria-preview-1@2x.jpg",
-        mobileTwo: "/assets/detail/desktop/image-galleria-preview-2@2x.jpg",
-        tabletOne: "/assets/detail/desktop/image-galleria-preview-1@2x.jpg",
-        tabletTwo: "/assets/detail/desktop/image-galleria-preview-2@2x.jpg",
-        desktopOne: "/assets/detail/desktop/image-galleria-preview-1@2x.jpg",
-        desktopTwo: "/assets/detail/desktop/image-galleria-preview-2@2x.jpg",
+        mobileOne: "/assets/detail/desktop/image-galleria-preview-1@2x.webp",
+        mobileTwo: "/assets/detail/desktop/image-galleria-preview-2@2x.webp",
+        tabletOne: "/assets/detail/desktop/image-galleria-preview-1@2x.webp",
+        tabletTwo: "/assets/detail/desktop/image-galleria-preview-2@2x.webp",
+        desktopOne: "/assets/detail/desktop/image-galleria-preview-1@2x.webp",
+        desktopTwo: "/assets/detail/desktop/image-galleria-preview-2@2x.webp",
       },
     },
     previous: "Coffee",
@@ -75,22 +75,25 @@ export const portfoliodata = [
       "This was a small project which mostly consisted of HTML and CSS. I built a fully-responsive landing page. The only JavaScript this project required was to enable the toggling of the mobile navigation.",
     images: {
       index: {
-        mobile: "/assets/portfolio/desktop/image-portfolio-planetfacts@2x.jpg",
-        tablet: "/assets/portfolio/desktop/image-portfolio-planetfacts@2x.jpg",
-        desktop: "/assets/portfolio/desktop/image-portfolio-planetfacts@2x.jpg",
+        mobile: "/assets/portfolio/desktop/image-portfolio-planetfacts@2x.webp",
+        tablet: "/assets/portfolio/desktop/image-portfolio-planetfacts@2x.webp",
+        desktop:
+          "/assets/portfolio/desktop/image-portfolio-planetfacts@2x.webp",
       },
       detailhero: {
-        mobile: "/assets/detail/desktop/image-planetfacts-hero@2x.jpg",
-        tablet: "/assets/detail/desktop/image-planetfacts-hero@2x.jpg",
-        desktop: "/assets/detail/desktop/image-planetfacts-hero@2x.jpg",
+        mobile: "/assets/detail/desktop/image-planetfacts-hero@2x.webp",
+        tablet: "/assets/detail/desktop/image-planetfacts-hero@2x.webp",
+        desktop: "/assets/detail/desktop/image-planetfacts-hero@2x.webp",
       },
       preview: {
-        mobileOne: "/assets/detail/desktop/image-planetfacts-preview-1@2x.jpg",
-        mobileTwo: "/assets/detail/desktop/image-planetfacts-preview-2@2x.jpg",
-        tabletOne: "/assets/detail/desktop/image-planetfacts-preview-1@2x.jpg",
-        tabletTwo: "/assets/detail/desktop/image-planetfacts-preview-2@2x.jpg",
-        desktopOne: "/assets/detail/desktop/image-planetfacts-preview-1@2x.jpg",
-        desktopTwo: "/assets/detail/desktop/image-planetfacts-preview-2@2x.jpg",
+        mobileOne: "/assets/detail/desktop/image-planetfacts-preview-1@2x.webp",
+        mobileTwo: "/assets/detail/desktop/image-planetfacts-preview-2@2x.webp",
+        tabletOne: "/assets/detail/desktop/image-planetfacts-preview-1@2x.webp",
+        tabletTwo: "/assets/detail/desktop/image-planetfacts-preview-2@2x.webp",
+        desktopOne:
+          "/assets/detail/desktop/image-planetfacts-preview-1@2x.webp",
+        desktopTwo:
+          "/assets/detail/desktop/image-planetfacts-preview-2@2x.webp",
       },
     },
     previous: "Galleria",
@@ -104,22 +107,22 @@ export const portfoliodata = [
       "This project was built in pure HTML and CSS. I had mobile and desktop designs to work to and build it so that it was fully responsive. I took a mobile-first approach and used modern CSS like Flexbox and Grid for layout purposes.",
     images: {
       index: {
-        mobile: "/assets/portfolio/desktop/image-portfolio-netflix@2x.jpg",
-        tablet: "/assets/portfolio/desktop/image-portfolio-netflix@2x.jpg",
-        desktop: "/assets/portfolio/desktop/image-portfolio-netflix@2x.jpg",
+        mobile: "/assets/portfolio/desktop/image-portfolio-netflix@2x.webp",
+        tablet: "/assets/portfolio/desktop/image-portfolio-netflix@2x.webp",
+        desktop: "/assets/portfolio/desktop/image-portfolio-netflix@2x.webp",
       },
       detailhero: {
-        mobile: "/assets/detail/desktop/image-netflix-hero@2x.jpg",
-        tablet: "/assets/detail/desktop/image-netflix-hero@2x.jpg",
-        desktop: "/assets/detail/desktop/image-netflix-hero@2x.jpg",
+        mobile: "/assets/detail/desktop/image-netflix-hero@2x.webp",
+        tablet: "/assets/detail/desktop/image-netflix-hero@2x.webp",
+        desktop: "/assets/detail/desktop/image-netflix-hero@2x.webp",
       },
       preview: {
-        mobileOne: "/assets/detail/desktop/image-netflix-preview-1@2x.jpg",
-        mobileTwo: "/assets/detail/desktop/image-netflix-preview-2@2x.jpg",
-        tabletOne: "/assets/detail/desktop/image-netflix-preview-1@2x.jpg",
-        tabletTwo: "/assets/detail/desktop/image-netflix-preview-2@2x.jpg",
-        desktopOne: "/assets/detail/desktop/image-netflix-preview-1@2x.jpg",
-        desktopTwo: "/assets/detail/desktop/image-netflix-preview-2@2x.jpg",
+        mobileOne: "/assets/detail/desktop/image-netflix-preview-1@2x.webp",
+        mobileTwo: "/assets/detail/desktop/image-netflix-preview-2@2x.webp",
+        tabletOne: "/assets/detail/desktop/image-netflix-preview-1@2x.webp",
+        tabletTwo: "/assets/detail/desktop/image-netflix-preview-2@2x.webp",
+        desktopOne: "/assets/detail/desktop/image-netflix-preview-1@2x.webp",
+        desktopTwo: "/assets/detail/desktop/image-netflix-preview-2@2x.webp",
       },
     },
     previous: "Planets",
