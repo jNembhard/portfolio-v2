04a0234238849543ec66b9f9ee0678879850799e
jNembhard <nembhardjl@outlook.com>
2022-06-03 00:41:06 -0400

    update meta titles

--- components/atoms/NextHead.js
+++ components/atoms/NextHead.js
@@ -3,7 +3,7 @@ import Head from "next/head";
 export default function NextHead({ title, metadescription }) {
   return (
     <Head>
-      <title>Jason Nembhard | {title}</title>
+      <title>{title} | Jason Nembhard</title>
       <meta name="description" content={metadescription} />
       <link rel="icon" href="/favicon.png" />
     </Head>
